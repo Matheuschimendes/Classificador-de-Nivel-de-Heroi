7b4ad466fb6df87a1a602220459b510a7421fc8f
Curso de JS <matheuschiimenndes@gmail.com>
2023-11-22 15:56:03 -0300

    Introduzindo desafio de nivel de herois

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+// Classificador de herois
+// 1️⃣ Desafio Classificador de nível de Herói
+
+/*
+O Que deve ser utilizado
+    - Variáveis
+    - Operadores
+    - Laços de repetição
+    - Estruturas de decisões
+*/
+/*
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+*/
+
+
+// Definindo variáveis para o nome e a quantidade de experiência (XP) do herói
+const nomeHeroi = "Mancudo" 
+const xpHeroi = 1001
+
+let nivelHeroi;
+
+if(xpHeroi <= 1000){
+     nivelHeroi = "Ferro";
+}else if(xpHeroi >= 1001 && xpHeroi <= 2000){
+    nivelHeroi = "Bronze";
+}else if(xpHeroi >= 2001 && xpHeroi <= 5000){
+    nivelHeroi = "Prata"   
+}else if(xpHeroi >= 6001 && xpHeroi <= 7000){
+    nivelHeroi = "Ouro"
+}else if (xpHeroi >= 7001 && xpHeroi <= 8000){
+    nivelHeroi = "Platina"
+}else if(xpHeroi >= 8001 && xpHeroi <= 9000){
+    nivelHeroi = "Ascendente"
+}else if(xpHeroi >= 9001 && xpHeroi <= 1000){
+    nivelHeroi = "Imortal"
+}else if(xpHeroi >= 10001){
+    nivelHeroi = "Radiante"
+}
+
+// Exibindo a mensagem com o nível do herói
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
+
